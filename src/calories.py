62c830a8b76833d744b2c0e11226eb066f2308d3
sean green <seanrgreen35@gmail.com>
2020-04-30 12:52:11 -0400

    More Changes

--- src/calories.py
+++ src/calories.py
@@ -1,193 +1,166 @@
 import sys 
-p_male = { }
-p_female = { }
-cal = { }
-cal_need = { }
-fat = { }
-carbs = { }
-pro = { }
-g_fat = { }
-g_carbs = { }
-g_pro = { }
-output = { }
+from main import request
 
-def male_stationary(mass,height,age,const):
-    output["p_male"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
-    output["cal"] = 1.2 * output["p_male"]
+def stationary(mass,height,age,const):
+    output = { }
+    if request.form["gender"] == "male":
+        output["p"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
+    else:
+        output["p"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
+    output["cal"] = 1.2 * output["p"]
     output["cal_need"] = output["cal"] + const
     return output
 
-def female_stationary(mass,height,age,const):
-    output["p_female"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
-    output["cal"] = 1.2 * output["p_female"]
+def light(mass,height,age,const):
+    output = { }
+    if request.form["gender"] == "male":
+        output["p"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
+    else:
+        output["p"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
+    output["cal"] = 1.25 * output["p"]
     output["cal_need"] = output["cal"] + const
     return output
 
-def male_light(mass,height,age,const):
-    output["p_male"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
-    output["cal"] = 1.25 * output["p_male"]
+def moderate(mass,height,age,const):
+    output = { }
+    if request.form["gender"] == "male":
+        output["p"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
+    else:
+        output["p"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
+    output["cal"] = 1.5 * output["p"]
     output["cal_need"] = output["cal"] + const
     return output
 
-def female_light(mass,height,age,const):
-    output["p_female"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
-    output["cal"] = 1.25 * output["p_female"]
+def active(mass,height,age,const):
+    output = { }
+    if request.form["gender"] == "male":
+        output["p"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
+    else:
+        output["p"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
+    output["cal"] = 1.6 * output["p"]
     output["cal_need"] = output["cal"] + const
     return output
 
-def male_moderate(mass,height,age,const):
-    output["p_male"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
-    output["cal"] = 1.5 * output["p_male"]
+def extreme(mass,height,age,const):
+    output = { }
+    if request.form["gender"] == "male":
+        output["p"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
+    else:
+        output["p"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
+    output["cal"] = 1.75 * output["p"]
     output["cal_need"] = output["cal"] + const
     return output
-
-def female_moderate(mass,height,age,const):
-    output["p_female"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
-    output["cal"] = 1.5 * output["p_female"]
-    output["cal_need"] = output["cal"] + const
-    return output
-
-def male_active(mass,height,age,const):
-    output["p_male"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
-    output["cal"] = 1.6 * output["p_male"]
-    output["cal_need"] = output["cal"] + const
-    return output
-
-def female_active(mass,height,age,const):
-    output["p_female"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
-    output["cal"] = 1.6 * output["p_female"]
-    output["cal_need"] = output["cal"] + const
-    return output
-
-def male_extreme(mass,height,age,const):
-    output["p_male"] = 13.397*float(mass) + 4.799*float(height) - 5.677*float(age) + 88.362 #BMR
-    output["cal"] = 1.76 * output["p_male"]
-    output["cal_need"] = output["cal"] + const
-    return output
-
-def female_extreme(mass,height,age,const):
-    output["p_female"] = 9.247*float(mass) + 3.098*float(height) - 4.330*float(age) + 447.593 #BMR
-    output["cal"] = 1.75 * output["p_female"]
-    output["cal_need"] = output["cal"] + const
-    return output
-    
 
 def calculator(gender, exer, goal, change, mass, height, age):
-
+    output = { }
+    
 #male,stationary
-    if (exer == "stationary" or exer ==  "light" or exer == "moderate" or exer ==  "active" or exer ==  "extreme"
-        and goal =="add" or goal == "lose"
-        and change == "0.5" or change == "1"): 
-        
-        if gender == "male" and exer == "stationary" and goal == "add" and change == "0.5":
-            male_stationary(mass,height,age, 250)
-        elif gender == "male" and exer == "stationary" and goal == "add" and change == "1":
-            male_stationary(mass,height,age, 500)
-        elif gender == "male" and exer == "stationary" and goal == "lose" and change == "0.5":
-            male_stationary(mass,height,age, -250) 
-        elif gender == "male" and exer == "stationary" and goal == "lose" and change =="1":
-            male_stationary(mass,height,age, -500)
+    if gender == "male" and exer == "stationary" and goal == "add" and change == "0.5":
+        output.update(stationary(mass,height,age, 250))
+    elif gender == "male" and exer == "stationary" and goal == "add" and change == "1":
+        output.update(stationary(mass,height,age, 500))
+    elif gender == "male" and exer == "stationary" and goal == "lose" and change == "0.5":
+        output.update(stationary(mass,height,age, -250) )
+    elif gender == "male" and exer == "stationary" and goal == "lose" and change =="1":
+        output.update(stationary(mass,height,age, -500))
             
 #female, stationary 
-        elif gender == "female" and exer == "stationary" and  goal =="add" and change == "0.5":
-            female_stationary(mass,height,age, 250)
-        elif gender == "female" and exer == "stationary" and  goal =="add" and change == "1":
-            female_stationary(mass,height,age, 500)
-        elif gender == "female" and exer == "stationary" and  goal =="lose" and change == "0.5":
-            female_stationary(mass,height,age, -250)
-        elif gender == "female" and exer == "stationary" and  goal =="lose" and change == "1":
-            female_stationary(mass,height,age, -500)
+    elif gender == "female" and exer == "stationary" and  goal =="add" and change == "0.5":
+        output.update(stationary(mass,height,age, 250))
+    elif gender == "female" and exer == "stationary" and  goal =="add" and change == "1":
+        output.update(stationary(mass,height,age, 500))
+    elif gender == "female" and exer == "stationary" and  goal =="lose" and change == "0.5":
+        output.update(stationary(mass,height,age, -250))
+    elif gender == "female" and exer == "stationary" and  goal =="lose" and change == "1":
+        output.update(stationary(mass,height,age, -500))
 
 # male light
-        elif gender == "male" and exer == "light" and goal == "add" and change == "0.5":
-            male_light(mass,height,age, 250)
-        elif gender == "male" and exer == "light" and goal == "add" and change == "1":
-            male_light(mass,height,age, 500)
-        elif gender == "male" and exer == "light" and goal == "lose" and change == "0.5":
-            male_light(mass,height,age, -250)            
-        elif gender == "male" and exer == "light" and goal == "lose" and change =="1":
-            male_light(mass,height,age, -500)
+    elif gender == "male" and exer == "light" and goal == "add" and change == "0.5":
+        output.update(light(mass,height,age, 250))
+    elif gender == "male" and exer == "light" and goal == "add" and change == "1":
+        output.update(light(mass,height,age, 500))
+    elif gender == "male" and exer == "light" and goal == "lose" and change == "0.5":
+        output.update(light(mass,height,age, -250))          
+    elif gender == "male" and exer == "light" and goal == "lose" and change =="1":
+        output.update(light(mass,height,age, -500))
             
 #female, light  
-        elif gender == "female" and exer == "light" and  goal =="add" and change == "0.5":
-            female_light(mass,height,age,250)
-        elif gender == "female" and exer == "light" and  goal =="add" and change == "1":
-            female_light(mass,height,age,500)
-        elif gender == "female" and exer == "light" and  goal =="lose" and change == "0.5":
-            female_light(mass,height,age,-250)
-        elif gender == "female" and exer == "light" and  goal =="lose" and change == "1":
-            female_light(mass,height,age,-500)
+    elif gender == "female" and exer == "light" and  goal =="add" and change == "0.5":
+        output.update(light(mass,height,age,250))
+    elif gender == "female" and exer == "light" and  goal =="add" and change == "1":
+        output.update(light(mass,height,age,500))
+    elif gender == "female" and exer == "light" and  goal =="lose" and change == "0.5":
+        output.update(light(mass,height,age,-250))
+    elif gender == "female" and exer == "light" and  goal =="lose" and change == "1":
+        output.update(light(mass,height,age,-500))
 
 # male moderate
-        elif gender == "male" and exer == "moderate" and goal == "add" and change == "0.5":
-            male_moderate(mass,height,age,250)
-        elif gender == "male" and exer == "moderate" and goal == "add" and change == "1":
-            male_moderate(mass,height,age,500)
-        elif gender == "male" and exer == "moderate" and goal == "lose" and change == "0.5":
-            male_moderate(mass,height,age,-250)  
-        elif gender == "male" and exer == "moderate" and goal == "lose" and change =="1":
-            male_moderate(mass,height,age,-500)
+    elif gender == "male" and exer == "moderate" and goal == "add" and change == "0.5":
+        output.update(moderate(mass,height,age,250))
+    elif gender == "male" and exer == "moderate" and goal == "add" and change == "1":
+        output.update(moderate(mass,height,age,500))
+    elif gender == "male" and exer == "moderate" and goal == "lose" and change == "0.5":
+        output.update(moderate(mass,height,age,-250)  )
+    elif gender == "male" and exer == "moderate" and goal == "lose" and change =="1":
+        output.update(moderate(mass,height,age,-500))
             
 #female, moderate 
-        elif gender == "female" and exer == "moderate" and  goal =="add" and change == "0.5":
-            female_moderate(mass,height,age,250)
-        elif gender == "female" and exer == "moderate" and  goal =="add" and change == "1":
-            female_moderate(mass,height,age,500)
-        elif gender == "female" and exer == "moderate" and  goal =="lose" and change == "0.5":
-            female_moderate(mass,height,age,-250)
-        elif gender == "female" and exer == "moderate" and  goal =="lose" and change == "1":
-            female_moderate(mass,height,age,-500)
+    elif gender == "female" and exer == "moderate" and  goal =="add" and change == "0.5":
+        output.update(moderate(mass,height,age,250))
+    elif gender == "female" and exer == "moderate" and  goal =="add" and change == "1":
+        output.update(moderate(mass,height,age,500))
+    elif gender == "female" and exer == "moderate" and  goal =="lose" and change == "0.5":
+        output.update(moderate(mass,height,age,-250))
+    elif gender == "female" and exer == "moderate" and  goal =="lose" and change == "1":
+        output.update(moderate(mass,height,age,-500))
 
 # male active
-        elif gender == "male" and exer == "active" and goal == "add" and change == "0.5":
-            male_active(mass,height,age,250)
-        elif gender == "male" and exer == "active" and goal == "add" and change == "1":
-            male_active(mass,height,age,500)
-        elif gender == "male" and exer == "active" and goal == "lose" and change == "0.5":
-            male_active(mass,height,age,-250)   
-        elif gender == "male" and exer == "active" and goal == "lose" and change =="1":
-            male_active(mass,height,age,-500)
+    elif gender == "male" and exer == "active" and goal == "add" and change == "0.5":
+        output.update(active(mass,height,age,250))
+    elif gender == "male" and exer == "active" and goal == "add" and change == "1":
+        output.update(active(mass,height,age,500))
+    elif gender == "male" and exer == "active" and goal == "lose" and change == "0.5":
+        output.update(active(mass,height,age,-250) )  
+    elif gender == "male" and exer == "active" and goal == "lose" and change =="1":
+        output.update(active(mass,height,age,-500))
             
 #female, active 
-        elif gender == "female" and exer == "active" and  goal =="add" and change == "0.5":
-            female_active(mass,height,age,250)
-        elif gender == "female" and exer == "active" and  goal =="add" and change == "1":
-            female_active(mass,height,age,500)
-        elif gender == "female" and exer == "active" and  goal =="lose" and change == "0.5":
-            female_active(mass,height,age,-250)
-        elif gender == "female" and exer == "active" and  goal =="lose" and change == "1":
-            female_active(mass,height,age,-500)
+    elif gender == "female" and exer == "active" and  goal =="add" and change == "0.5":
+        output.update(active(mass,height,age,250))
+    elif gender == "female" and exer == "active" and  goal =="add" and change == "1":
+        output.update(active(mass,height,age,500))
+    elif gender == "female" and exer == "active" and  goal =="lose" and change == "0.5":
+        output.update(active(mass,height,age,-250))
+    elif gender == "female" and exer == "active" and  goal =="lose" and change == "1":
+        output.update(active(mass,height,age,-500))
             
 # male extreme
-        elif gender == "male" and exer == "extreme" and goal == "add" and change == "0.5":
-            male_extreme(mass,height,age,250)
-        elif gender == "male" and exer == "extreme" and goal == "add" and change == "1":
-            male_extreme(mass,height,age,500)
-        elif gender == "male" and exer == "extreme" and goal == "lose" and change == "0.5":
-            male_extreme(mass,height,age,-250)  
-        elif gender == "male" and exer == "extreme" and goal == "lose" and change =="1":
-            male_extreme(mass,height,age,-500)
+    elif gender == "male" and exer == "extreme" and goal == "add" and change == "0.5":
+        output.update(extreme(mass,height,age,250))
+    elif gender == "male" and exer == "extreme" and goal == "add" and change == "1":
+        output.update(extreme(mass,height,age,500))
+    elif gender == "male" and exer == "extreme" and goal == "lose" and change == "0.5":
+        output.update(extreme(mass,height,age,-250)  )
+    elif gender == "male" and exer == "extreme" and goal == "lose" and change =="1":
+        output.update(extreme(mass,height,age,-500))
             
 #female extreme
-        elif gender == "female" and exer == "extreme" and  goal =="add" and change == "0.5":
-            female_extreme(mass,height,age,250)
-        elif gender == "female" and exer == "extreme" and  goal =="add" and change == "1":
-            female_extreme(mass,height,age,500)
-        elif gender == "female" and exer == "extreme" and  goal =="lose" and change == "0.5":
-            female_extreme(mass,height,age,-250)
-        elif gender == "female" and exer == "extreme" and  goal =="lose" and change == "1":
-            female_extreme(mass,height,age,-500)
-
-        else:
-            sys.exit("an unknown error has occured")
+    elif gender == "female" and exer == "extreme" and  goal =="add" and change == "0.5":
+        output.update(extreme(mass,height,age,250))
+    elif gender == "female" and exer == "extreme" and  goal =="add" and change == "1":
+        output.update(extreme(mass,height,age,500))
+    elif gender == "female" and exer == "extreme" and  goal =="lose" and change == "0.5":
+        output.update(extreme(mass,height,age,-250))
+    elif gender == "female" and exer == "extreme" and  goal =="lose" and change == "1":
+        output.update(extreme(mass,height,age,-500))
 
     else:
         sys.exit("an unknown error has occured")
-        
+
     if gender == "male":
-        p_male = output["p_male"]
+        p_male = output["p"]
     else:
-        p_female = output["p_female"]
+        p_female = output["p"]
     cal = output["cal"]
     cal_need = output["cal_need"]
     carbs = 0.5 * output["cal_need"] #cals in carbs
@@ -196,7 +169,6 @@ def calculator(gender, exer, goal, change, mass, height, age):
     g_fat = fat/9
     pro = 0.2 * output["cal_need"] #cals in pro
     g_pro = pro/4
-
 
     return { 'BMR': round(p_male) if gender =="male" else round(p_female),
              "Total Cals used": round(cal),
